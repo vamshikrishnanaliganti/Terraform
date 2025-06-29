174a12826b7c1335871fde6f28a6a040c6d0fbb3
vamshikrishnanaliganti <vamshikrishnanaliganti.com>
2025-06-29 15:59:05 +0530

    import

---
+++ day4-import/main.tf
@@ -0,0 +1,18 @@
+
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0c803b171269e2d72"
+  key_name      = "testkey"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name = "test1"
+  }
+
+}
