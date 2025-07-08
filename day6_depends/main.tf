0531a02cff8cc258be266bef4265e8a80871dbbd
vamshikrishnanaliganti <vamshikrishnanaliganti@gmail.com>
2025-07-09 00:25:45 +0530

    Update main.tf

--- day6_depends/main.tf
+++ day6_depends/main.tf
@@ -1,15 +1,15 @@
 
 
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0c803b171269e2d72"
-  key_name      = "testkey"
-  instance_type = "t2.micro"
-  availability_zone = "us-east-2b"
+  ami           = "ami-05ffe3c48a9991133"
+  key_name      = "vvv"
+  instance_type = "t2.nano"
+
 
   tags = {
     Name = "test1"
@@ -18,11 +18,7 @@ resource "aws_instance" "my_ec2" {
 }
 
 
-resource "aws_s3_bucket" "intel" {
-  bucket = "dependsonbucketactivatebucketraxyz"
 
-  depends_on = [ aws_instance.my_ec2 ]
-}
 
 
 
