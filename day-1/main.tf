3c87d28ef810c45f14f2abc7d70c9b56711df33e
vamshikrishnanaliganti <vamshikrishnanaliganti@gmail.com>
2025-07-08 23:57:30 +0530

    ads

--- day-1/main.tf
+++ day-1/main.tf
@@ -1,12 +1,12 @@
-resource "aws_s3_bucket" "mybucket" {
-  bucket = "myterraformbucket2025new"
-}
+
+
+
 
 
 resource "aws_instance" "dev" {
-  ami           = var.amiid
-  instance_type = var.instance_type
-  key_name      = var.key
+  ami           = "ami-0d1b5a8c13042c939"
+  instance_type = "t2.micro"
+  key_name      = "testkey"
   tags = {
         Name = "dev"
     }
--- day-1/variables.tf
+++ day-1/variables.tf
@@ -12,3 +12,14 @@ variable "instance_type" {
     type    = string
     default = ""
 }
+
+# variable "db_instance_username" {
+#   type        = string
+#   description = "Username for RDS"
+# }
+
+# variable "db_instance_password" {
+#   type        = string
+#   description = "Password for RDS"
+#   sensitive   = true
+# }
